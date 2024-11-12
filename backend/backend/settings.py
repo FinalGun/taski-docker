da5c9f2b0c4fe5e841146dd26696de20076c6b17
Александр Игошев <aigoshev69@mail.ru>
2024-11-12 19:16:22 +0500

    Add workflow with PEP8 linting2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,19 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = ('django-insecure-j_89af+30&&4qm*8z9_'
+              '(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123', 'localhost', '89.169.168.35', 'ya-task.myftp.org']
+ALLOWED_HOSTS = [
+    '123.123.123.123',
+    'localhost', '89.169.168.35',
+    'ya-task.myftp.org'
+]
 
 
 # Application definition
@@ -79,16 +84,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
     },
 ]
 
